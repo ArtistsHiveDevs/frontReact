fcc9e09fbb7d6f92f9d12cd146e587bb7eb6620f
Carlos Navarrete <navarretecarp@globalhitss.com>
2022-12-10 16:18:48 -0500

    Creación del despliegue semiautomático

--- src/constants/routes.constants.ts
+++ src/constants/routes.constants.ts
@@ -3,3 +3,11 @@ export enum PATHS {
   EVENTS = "/events",
   NOT_FOUND = "*",
 }
+
+export enum SUB_PATHS {
+  ELEMENT_DETAILS = "details",
+}
+
+export enum URL_PARAMETER_NAMES {
+  ELEMENT_ID = "id",
+}

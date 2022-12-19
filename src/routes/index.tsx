aa2e833819e38c44d2d715a31b1eeffaaefe3071
Carlos Navarrete <navarretecarp@globalhitss.com>
2022-12-19 11:42:18 -0500

    Se adelanta implementación del home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { PATHS } from "../constants";
+import { PATHS } from "~/constants";
 
 // Lazy loading
-const HomePage = lazy(() => import("../components/Pages/HomePage"));
-const EventPage = lazy(() => import("../components/Pages/EventsPage"));
-const NotFoundPage = lazy(() => import("../components/Pages/NotFoundPage"));
+const HomePage = lazy(() => import("~/components/Pages/HomePage"));
+const EventPage = lazy(() => import("~/components/Pages/EventsPage"));
+const NotFoundPage = lazy(() => import("~/components/Pages/NotFoundPage"));
 
 export const RoutesApp: React.FC = () => {
   return (

d598793007541baa9a53dd2ce54a6dff468b20f4
Carlos Navarrete <navarretecarp@globalhitss.com>
2022-12-19 10:10:38 -0500

    Traducciones al home en varios idiomas

--- src/translations/de.tsx
+++ src/translations/de.tsx
@@ -3,6 +3,13 @@ export const DeMessages = {
     name: "App DE",
     loading: "Wird geladen",
     pages: {
+      HomePage: {
+        welcome: "Willkomen",
+        news: "Neu",
+        artists: "Artists",
+        events: "Ereignisses",
+        places: "Örtlichkeit",
+      },
       EventsPage: {
         title: "Events Page",
       },

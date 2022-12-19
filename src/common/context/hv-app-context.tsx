2239c55b621409f59899098d38c067a7dd95f588
Carlos Navarrete <navarretecarp@globalhitss.com>
2022-12-19 10:26:06 -0500

    config i18n 'es' as default

--- src/common/context/hv-app-context.tsx
+++ src/common/context/hv-app-context.tsx
@@ -1,13 +1,17 @@
 import { useState, useEffect, createContext } from "react";
 
+import { appMessages } from "../../translations";
+
 import {
   IHvAppContext,
   IHvAppContextProvider,
 } from "./models/hv-app-context.model";
 
+const defaultLang: string = localStorage?.currentLang || "es";
+
 export const HvAppContext = createContext<IHvAppContext>({
-  lang: "",
-  messages: {},
+  lang: defaultLang,
+  messages: appMessages[defaultLang],
   setLang: () => {},
 });
 
@@ -15,7 +19,7 @@ export const HvAppContextProvider = ({
   children,
   appMessages,
 }: IHvAppContextProvider) => {
-  const [lang, setLang] = useState(localStorage?.currentLang || "en");
+  const [lang, setLang] = useState(defaultLang);
   const messages = appMessages[lang] || {};
 
   function onSetLang(nextLang: string) {
@@ -25,7 +29,7 @@ export const HvAppContextProvider = ({
 
   useEffect(() => {
     if (!lang) {
-      const nextLang = localStorage?.currentLang || "en";
+      const nextLang = defaultLang;
 
       localStorage.currentLang = nextLang;
       setLang(nextLang);

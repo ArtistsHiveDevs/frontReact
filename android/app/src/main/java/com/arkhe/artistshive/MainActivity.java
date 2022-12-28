d2ce49431839fd6f21a7eee7af3df2c60a1d30ee
Carlos Navarrete <navarretecarp@globalhitss.com>
2022-12-28 18:59:49 -0500

    Se añade Capacitor, ios y android

---
+++ android/app/src/main/java/com/arkhe/artistshive/MainActivity.java
@@ -0,0 +1,5 @@
+package com.arkhe.artistshive;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.arkhe.artistshive',
+  appName: 'Artists Hive',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;

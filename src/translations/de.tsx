7bc63e6afefdcc51758bc9132c223e415390699c
Carlos Navarrete <navarretecarp@globalhitss.com>
2023-01-02 12:13:00 -0500

    Simplificación Artist Details v1

--- src/translations/de.tsx
+++ src/translations/de.tsx
@@ -3,6 +3,101 @@ export const DeMessages = {
     name: "App DE",
     loading: "Wird geladen",
     pages: {
+      ArtistsPages: {
+        ArtistsDetailsPage: {
+          subpages: {
+            general: {
+              name: "Allgemeine",
+              sections: {
+                general: {
+                  name: "Allgemeine Informationen",
+                  attributes: {
+                    description: "",
+                    since: "Seit",
+                    home_city: "Stadt der Niederlassung",
+                    categories: "Kategorien",
+                    genres: "Genres",
+                    spoken_languages: "Gesprochene Sprachen",
+                    stage_languages: "Sprachen auf der Bühne",
+                    arts_languages: "Sprachen der Künste",
+                  },
+                },
+                contact: {
+                  name: "Kontakt",
+                  attributes: {
+                    website: "Webseite",
+                    email: "e-mail",
+                    phone: "Telefon",
+                    mobile_phone: "Mobiltelefon",
+                    whatsapp: "WhatsApp",
+                  },
+                },
+                social_networks: {
+                  name: "Soziale Netzwerke",
+                },
+                record_label: {
+                  name: "Plattenfirma",
+                },
+                members: {
+                  name: "Mitglieder",
+                },
+              },
+            },
+            arts: {
+              name: "Künste",
+              sections: {
+                discography: {
+                  name: "Diskographie",
+                  attributes: {
+                    albums: "Alben",
+                    dvd_video: "DVD / Video",
+                  },
+                },
+                media_channels: {
+                  name: "Medienkanäle",
+                },
+                gallery: {
+                  name: "Galerie",
+                  attributes: {
+                    photos: "Fotos",
+                    video: "Video",
+                  },
+                },
+                awards: {
+                  name: "Auszeichnungen",
+                },
+              },
+            },
+            social: {
+              name: "Sozial",
+              sections: {
+                social_network_presence: {
+                  name: "Präsenz in sozialen Netzwerken",
+                },
+                stats: {
+                  name: "Statistiken",
+                  attributes: {
+                    general_rate: "Allgemeine Qualifikation",
+                    followers: "Anhänger",
+                    event_followers: "Follower-Ereignis",
+                  },
+                },
+              },
+            },
+            shows: {
+              name: "Shows",
+              sections: {
+                next_shows: {
+                  name: "Nächsten Shows",
+                },
+                past_shows: {
+                  name: "Vergangenen Shows",
+                },
+              },
+            },
+          },
+        },
+      },
       HomePage: {
         welcome: "Willkomen",
         news: "Neu",

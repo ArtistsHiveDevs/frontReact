1cb306d9a9e46a6e032b1e51693d3374348c5773
Samuel Santiago <samuel.santiago95@gmail.com>
2023-01-04 23:36:21 -0500

    Se crea componente para mostrar mapa

---
+++ src/components/shared/mapPrinter/mapViewer/index.tsx
@@ -0,0 +1,27 @@
+import "./index.scss";
+import { useEffect, useRef } from "react";
+
+const MapViewer = (props: any) => {
+  const ref = useRef();
+  const { data } = props;
+
+  useEffect(() => {
+    let map = new window.google.maps.Map(ref.current, {
+      zoom: data?.zoom,
+      center: data?.center,
+      ...data?.anotherOpts,
+    });
+
+    data?.marksLocation?.forEach((position: any) => {
+      console.log("entra con marca: ", position);
+      new google.maps.Marker({
+        position,
+        map: map,
+      });
+    });
+  });
+
+  return <div ref={ref} id="map-viewer"></div>;
+};
+
+export default MapViewer;

8edd37733ba4c34baaa972ddd9fabcedf380dd3c
Carlos Navarrete <navarretecarp@globalhitss.com>
2023-01-14 20:28:18 -0500

    Uso de ProfileTabsPage en EventDetails y correcciones en ArtistDetails y PlaceDetails

---
+++ src/components/Pages/EventsPage/EventDetailsPage/config-event-detail.tsx
@@ -0,0 +1,277 @@
+import {
+  ProfileComponentTypes,
+  ProfileDetailsSubpage,
+} from "~/components/shared/organisms/ProfileTabsPage/profile-details.def";
+import { EventModel } from "~/models/domain/event/event.model";
+import { PlaceModel } from "~/models/domain/place/place.model";
+
+export const EVENT_DETAIL_SUB_PAGE_CONFIG: ProfileDetailsSubpage[] = [
+  {
+    name: "general",
+    sections: [
+      {
+        name: "general",
+        components: [
+          {
+            componentName: ProfileComponentTypes.ATTRIBUTES_ICON_FIELDS,
+            data: {
+              attributes: [
+                {
+                  icon: "FaRegCalendarAlt",
+                  name: "timetable__initial_date",
+                },
+                {
+                  icon: "TbDoorEnter",
+                  name: "timetable__openning_doors",
+                },
+                {
+                  icon: "IoTimeOutline",
+                  name: "initial_time",
+                },
+                {
+                  icon: "FaMapMarkerAlt",
+                  name: "place.name",
+                  emptyTitle: true,
+                  value: (event: EventModel) => {
+                    return (
+                      <>
+                        {event.place?.name} <br />
+                        {event.place?.address} <br />
+                        {event.place?.city} <br />
+                        {event.place?.country}
+                      </>
+                    );
+                  },
+                },
+                {
+                  icon: "IoTicketOutline",
+                  name: "tickets_website",
+                  emptyTitle: true,
+                },
+                {
+                  icon: "IoTimeOutline",
+                  name: "minimumAge",
+                },
+                {
+                  icon: "BsInfoCircleFill",
+                  name: "promoter",
+                },
+                {
+                  icon: "IoBarcodeOutline",
+                  name: "national_code",
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        name: "description",
+        components: [
+          {
+            componentName: ProfileComponentTypes.HTML_CONTENT,
+            data: {
+              attribute_content: "description",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "artists",
+    sections: [
+      {
+        name: "main_artists",
+        components: [
+          {
+            componentName: ProfileComponentTypes.PROFILE_THUMBNAIL_CARD,
+            data: {
+              data_source: "main_artists",
+            },
+            clickHandlerName: "onNavigateToEntity",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "place",
+    sections: [
+      {
+        id: "place",
+        components: [
+          {
+            componentName: ProfileComponentTypes.PROFILE_THUMBNAIL_CARD,
+            data: {
+              data_source: "place",
+              footer: {
+                components: [
+                  {
+                    componentName: ProfileComponentTypes.ATTRIBUTES_ICON_FIELDS,
+                    data: {
+                      data_source: "place",
+                      attributes: [
+                        {
+                          name: "address",
+                        },
+                        {
+                          icon: "FaCity",
+                          name: "city",
+                          emptyTitle: true,
+                          value: (place: PlaceModel) => {
+                            return <>{`${place.city}, ${place.country}`}</>;
+                          },
+                        },
+                        {
+                          name: "website",
+                        },
+                        {
+                          name: "phone",
+                        },
+                        {
+                          name: "mobile_phone",
+                        },
+                        {
+                          name: "whatsapp",
+                        },
+                        {
+                          name: "email",
+                        },
+                        {
+                          name: "facebook",
+                        },
+                        {
+                          name: "instagram",
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+            },
+            clickHandlerName: "onNavigateToEntity",
+          },
+        ],
+      },
+      {
+        name: "location",
+        components: [
+          {
+            componentName: ProfileComponentTypes.MAP,
+            data: {
+              data_source: "place",
+              lat: "latitude",
+              lng: "longitude",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "contact",
+    sections: [
+      {
+        name: "contact",
+        components: [
+          {
+            componentName: ProfileComponentTypes.ATTRIBUTES_ICON_FIELDS,
+            data: {
+              attributes: [
+                {
+                  name: "website",
+                },
+                {
+                  name: "phone",
+                },
+                {
+                  name: "mobile_phone",
+                },
+                {
+                  name: "email",
+                },
+              ],
+            },
+          },
+        ],
+      },
+      {
+        name: "social_networks",
+        components: [
+          {
+            componentName: ProfileComponentTypes.ATTRIBUTES_ICON_FIELDS,
+            data: {
+              attributes: [
+                {
+                  name: "facebook",
+                },
+                {
+                  name: "instagram",
+                },
+                {
+                  name: "twitter",
+                },
+                {
+                  name: "youtube",
+                },
+                {
+                  name: "spotify",
+                },
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "extra_info",
+    sections: [
+      {
+        name: "additional_info",
+        components: [
+          {
+            componentName: ProfileComponentTypes.HTML_CONTENT,
+            data: {
+              attribute_content: "additional_info",
+            },
+          },
+        ],
+      },
+      {
+        name: "dress_code",
+        components: [
+          {
+            componentName: ProfileComponentTypes.HTML_CONTENT,
+            data: {
+              attribute_content: "dress_code",
+            },
+          },
+        ],
+      },
+      {
+        name: "discounts",
+        components: [
+          {
+            componentName: ProfileComponentTypes.HTML_CONTENT,
+            data: {
+              attribute_content: "discounts",
+            },
+          },
+        ],
+      },
+      {
+        name: "promoter",
+        components: [
+          {
+            componentName: ProfileComponentTypes.HTML_CONTENT,
+            data: {
+              attribute_content: "promoter",
+            },
+          },
+        ],
+      },
+    ],
+  },
+];
--- src/components/Pages/EventsPage/EventDetailsPage/index.tsx
+++ src/components/Pages/EventsPage/EventDetailsPage/index.tsx
@@ -1,19 +1,20 @@
 import "./index.scss";
 
 import { useEffect, useState } from "react";
+import { Image } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEventsSlice } from "~/common/slices/events";
 import { selectEvents } from "~/common/slices/events/selectors";
 import { useI18n } from "~/common/utils";
 import DynamicIcons from "~/components/shared/DynamicIcons";
+import { ProfileTabsPage } from "~/components/shared/organisms/ProfileTabsPage/ProfileTabsPage";
 import VerifiedArtist from "~/components/shared/VerifiedArtist";
 import { PATHS, SUB_PATHS, URL_PARAMETER_NAMES } from "~/constants";
-import { EventModel } from "~/models/domain/event/event.model";
-import IconFieldReadOnly from "~/components/shared/atoms/IconField";
-import ProfileThumbnailCard from "~/components/shared/molecules/Profile/ProfileThumbnailCard";
 import { ArtistModel } from "~/models/domain/artist/artist.model";
-import { Image } from "react-bootstrap";
+import { EventModel } from "~/models/domain/event/event.model";
+import { PlaceModel } from "~/models/domain/place/place.model";
+import { EVENT_DETAIL_SUB_PAGE_CONFIG } from "./config-event-detail";
 
 const TRANSLATION_BASE_EVENT_DETAILS_PAGE: string =
   "app.pages.EventsPages.EventDetailsPage";
@@ -25,14 +26,12 @@ const EventDetailsPage = () => {
 
   // States
   const [currentEvent, setCurrentEvent] = useState<EventModel>(undefined);
-  const [generalInfoFields, setGeneralInfoFields] = useState([]);
-  const [artists, setArtists] = useState<ArtistModel[]>([]);
-  const [contactInfoFields, setContactInfoFields] = useState([]);
-  const [placeInfoFields, setPlaceInfoFields] = useState([]);
   const [requestsAreReady, setRequestesAreReady] = useState(false);
 
   const eventsList: EventModel[] = useSelector(selectEvents);
   const { actions: eventActions } = useEventsSlice();
+
+  const subPagesInfo = [...EVENT_DETAIL_SUB_PAGE_CONFIG];
 
   // Hooks
   const dispatch = useDispatch();
@@ -61,309 +60,54 @@ const EventDetailsPage = () => {
     }
   }, [eventsList]);
 
-  useEffect(() => {
-    if (currentEvent) {
-      setArtists([currentEvent.main_artist, currentEvent.guest_artist]);
-      setGeneralInfoFields([
-        {
-          icon: "FaRegCalendarAlt",
-          fieldName: translateText(
-            `${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.date`
-          ),
-          fieldValue: currentEvent?.timetable__initial_date,
-        },
-        {
-          icon: "TbDoorEnter",
-          fieldName: translateText(
-            `${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.openingDoors`
-          ),
-          fieldValue: currentEvent?.timetable__openning_doors,
-        },
-        {
-          icon: "IoTimeOutline",
-          fieldName: translateText(
-            `${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.initialTime`
-          ),
-          fieldValue: currentEvent?.timetable__guest_time,
-        },
-        {
-          icon: "FaMapMarkerAlt",
-          fieldName: "",
-          fieldValue: (
-            <>
-              {currentEvent.place.name} <br /> {currentEvent.place.address}
-              <br /> {currentEvent.place.city}
-            </>
-          ),
-        },
-        {
-          icon: "IoTicketOutline",
-          fieldName: "",
-          fieldValue: "https://www.tickets.com",
-        },
-        {
-          icon: "IoTimeOutline",
-          fieldName: translateText(
-            `${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.minimumAge`
-          ),
-          fieldValue: currentEvent?.timetable__guest_time,
-        },
-        {
-          icon: "BsInfoCircleFill",
-          fieldName: translateText(
-            `${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.promoter`
-          ),
-          fieldValue: currentEvent?.timetable__guest_time,
-        },
-        {
-          icon: "IoBarcodeOutline",
-          fieldName: translateText(
-            `${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.national_code`
-          ),
-          fieldValue: currentEvent?.timetable__guest_time,
-        },
-      ]);
-
-      setContactInfoFields([
-        {
-          fieldName: "facebook",
-          emptyTitle: true,
-          fieldValue: "asdasd",
-        },
-        {
-          icon: "GrInstagram",
-          fieldName: "",
-          fieldValue: `@${currentEvent?.timetable__guest_time}`,
-        },
-        {
-          icon: "GrTwitter",
-          fieldName: "",
-          fieldValue: `@${currentEvent?.timetable__guest_time}`,
-        },
-        {
-          icon: "GrYoutube",
-          fieldName: "",
-          fieldValue: `@${currentEvent?.timetable__guest_time}`,
-        },
-        {
-          icon: "GrSpotify",
-          fieldName: "",
-          fieldValue: `@${currentEvent?.timetable__guest_time}`,
-        },
-        {
-          icon: "TfiWorld",
-          fieldName: "",
-          fieldValue: `@${currentEvent?.timetable__guest_time}`,
-        },
-        {
-          icon: "FaPhoneAlt",
-          fieldName: "",
-          fieldValue: `@${currentEvent?.timetable__guest_time}`,
-        },
-        {
-          icon: "HiDevicePhoneMobile",
-          fieldName: "",
-          fieldValue: `@${currentEvent?.timetable__guest_time}`,
-        },
-        {
-          emptyTitle: true,
-          fieldName: "email",
-          fieldValue: `email@events.com`,
-        },
-      ]);
-
-      setPlaceInfoFields([
-        {
-          icon: "FaMapMarkerAlt",
-          fieldName: "",
-          fieldValue: currentEvent.place?.address,
-        },
-        {
-          icon: "FaCity",
-          fieldName: "",
-          fieldValue: `${currentEvent.place?.city}, ${currentEvent.place?.country}`,
-        },
-        {
-          icon: "FaPhoneAlt",
-          fieldName: "",
-          fieldValue: currentEvent.place?.phone,
-        },
-        {
-          icon: "FaWhatsapp",
-          fieldName: "",
-          fieldValue: currentEvent.place?.phone,
-        },
-      ]);
-    }
-  }, [currentEvent]);
-
-  // Helpers
-  function placeInfoThumbnailFooter() {
-    return placeInfoFields.map((field, index) => (
-      <IconFieldReadOnly
-        key={`${field.fieldName}-${index}`}
-        icon={field.icon}
-        fieldName={field.fieldName}
-        fieldValue={field.fieldValue}
-      />
-    ));
-  }
-
   function navigateTo(newEntity: PATHS, id: string = null) {
     navigate(`${newEntity}/${SUB_PATHS.ELEMENT_DETAILS}/${id}`);
   }
+
+  const handlers = {
+    onNavigateToEntity: (value: any) => {
+      const entityType = value.constructor.name;
+      let entity = undefined;
+      if (entityType === ArtistModel.name) {
+        entity = PATHS.ARTISTS;
+      } else if (entityType === EventModel.name) {
+        entity = PATHS.EVENTS;
+      } else if (entityType === PlaceModel.name) {
+        entity = PATHS.PLACES;
+      }
+      if (entity) {
+        navigateTo(entity, value.id);
+      }
+    },
+  };
 
   return (
     <>
       {currentEvent && (
         <>
-          <h1 className="event-title">
-            {currentEvent.name}{" "}
-            <VerifiedArtist verifiedStatus={currentEvent?.verified_status} />
-            <DynamicIcons iconName="FaHeart" size={25} color="#e30000" />
-          </h1>
-
-          <Image
-            alt={currentEvent.name}
-            src={currentEvent.photo}
-            fluid={true}
+          <ProfileTabsPage
+            entityName="Event"
+            entityData={currentEvent}
+            translation_base_path={TRANSLATION_BASE_EVENT_DETAILS_PAGE}
+            subpagesConfig={subPagesInfo}
+            handlers={handlers}
+            profileHeaderComponent={
+              <>
+                <h1 className="event-title">
+                  {currentEvent.name}{" "}
+                  <VerifiedArtist
+                    verifiedStatus={currentEvent?.verified_status}
+                  />
+                  <DynamicIcons iconName="FaHeart" size={25} color="#e30000" />
+                </h1>
+                <Image
+                  alt={currentEvent.name}
+                  src={currentEvent.photo}
+                  fluid={true}
+                />
+              </>
+            }
           />
-
-          <hr />
-          <h2 className="section-title">
-            {translateText(
-              `${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.general_information`
-            )}
-          </h2>
-          <div className="info-container">
-            {generalInfoFields.map((field, index) => (
-              <IconFieldReadOnly
-                key={`${field.fieldName}-${index}`}
-                icon={field.icon}
-                fieldName={field.fieldName}
-                fieldTitle={field.fieldName}
-                fieldValue={field.fieldValue}
-              />
-            ))}
-          </div>
-          <h2 className="section-title">
-            {translateText(
-              `${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.description`
-            )}
-          </h2>
-          <div className="info-container">
-            <p>
-              Te invitamos a que nos acompañes a la versión número 15 del Gran
-              Concierto de fin de año 2022, un concierto en vivo y en directo en
-              donde recibiremos el año nuevo con una recopilación de varias de
-              las obras que hemos presentado durante las ediciones anteriores.
-            </p>
-            <p>
-              Desde 2008, ininterrumpidamente se ha realizado esta gala musical
-              todos los 31 de diciembre, en donde el público capitalino disfruta
-              de lo más selecto de la música clásica. La interpretación estará a
-              cargo del Coro y Orquesta de la Fundación Orquesta Sinfónica de
-              Bogotá - FOSBO, bajo la dirección del maestro Carlos Agreda, y
-              como solistas invitados el pianista Mauricio Arias y la soprano
-              Beatriz Mora.
-            </p>
-            <p>
-              Te garantizamos una velada con un alto nivel artístico y en donde
-              el corazón de cada persona que no acompañe latirá y vibrará con
-              cada nota que emita la orquesta y el coro permitiendo así, meditar
-              sobre las cosas buenas y malas de este año 2022 e iniciar con un
-              nuevo espíritu el 2023.
-            </p>
-            <p>
-              La cita al Gran Concierto de fin de año es el próximo 31 de
-              diciembre a las 8:00 p.m. en nuestro Teatro Cafam.
-            </p>
-          </div>
-          <h2 className="section-title">
-            {translateText(`${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.artists`)}
-          </h2>
-
-          <div className="info-container">
-            {artists.map((artist, index) => (
-              <ProfileThumbnailCard
-                key={`${artist.id}-${index}`}
-                elementData={{
-                  id: artist.id,
-                  profile_pic: artist.profile_pic,
-                  name: artist.name,
-                  subtitle: artist.subtitle,
-                  verified_status: artist.verified_status,
-                }}
-                callbacks={{
-                  onClickCard: (elementData: any) =>
-                    navigateTo(PATHS.ARTISTS, elementData.id),
-                }}
-              />
-            ))}
-          </div>
-
-          <h2 className="section-title">
-            {translateText(`${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.contact`)}
-          </h2>
-          <div className="info-container">
-            {contactInfoFields.map((field, index) => (
-              <IconFieldReadOnly
-                key={`${field.fieldName}-${index}`}
-                icon={field.icon}
-                fieldName={field.fieldName}
-                fieldValue={field.fieldValue}
-              />
-            ))}
-          </div>
-          <h2 className="section-title">
-            {translateText(`${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.place`)}
-          </h2>
-          <div>
-            {currentEvent.place && (
-              <ProfileThumbnailCard
-                key={`${currentEvent.place?.id}`}
-                elementData={{
-                  id: currentEvent.place_id,
-                  profile_pic: currentEvent.place?.profile_pic,
-                  name: currentEvent.place?.name,
-                  verified_status: currentEvent.main_artist?.verified_status,
-                }}
-                footer={() => placeInfoThumbnailFooter()}
-                styles={{ avatar: "avatar-place" }}
-                callbacks={{
-                  onClickCard: (elementData: any) =>
-                    navigateTo(PATHS.PLACES, elementData.id),
-                }}
-              />
-            )}
-          </div>
-          <h2 className="section-title">
-            {translateText(
-              `${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.additionalInfo`
-            )}
-          </h2>
-          <div>
-            <ul>
-              <li>
-                Recuerde tener listo su tiquete de entrada impreso o el código
-                QR en el celular al momento del ingreso para agilizar la
-                entrada.
-              </li>
-              <li>El evento inicia puntualmente.</li>
-              <li>
-                No se permite el ingreso de personas en estado de embriaguez ni
-                efectos alucinógenos.
-              </li>
-            </ul>
-          </div>
-          <h2 className="section-title">
-            {translateText(`${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.discounts`)}
-          </h2>
-          <div></div>
-          <h2 className="section-title">
-            {translateText(`${TRANSLATION_BASE_EVENT_DETAILS_PAGE}.promoter`)}
-          </h2>
-          <div></div>
         </>
       )}
       {requestsAreReady && !currentEvent && (

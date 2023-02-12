7c0e0205c5b18e11522848dab518a60b6cdb0990
Carlos Navarrete <navarretecarp@globalhitss.com>
2023-02-12 17:32:01 -0500

    Se elimina la traducción de redes sociales

--- src/common/utils/hooks/useI18n.tsx
+++ src/common/utils/hooks/useI18n.tsx
@@ -1,10 +1,21 @@
 import { FormattedMessage, useIntl } from "react-intl";
+import { SocialNetworks } from "~/constants/social-networks.const";
 
 export const useI18n = () => {
   const { locale = "es", formatMessage } = useIntl();
 
   const translateText = (messageId: string) => {
-    return formatMessage({ id: messageId });
+    const subpaths = messageId?.split(".") || [];
+    let response = "";
+    if (subpaths.length) {
+      const socialNetworkName = SocialNetworks[subpaths[subpaths.length - 1]];
+      if (!socialNetworkName) {
+        response = formatMessage({ id: messageId });
+      } else {
+        response = socialNetworkName.title;
+      }
+    }
+    return response;
   };
 
   const getFormattedMessage = (messageId: any, messageValues: any) => {

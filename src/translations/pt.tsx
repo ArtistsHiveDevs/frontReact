070497789e5869799672502646c0af6d8647c152
Carlos Navarrete <navarretecarp@globalhitss.com>
2023-03-04 22:00:23 -0500

    Portuguese translation

---
+++ src/translations/pt.tsx
@@ -0,0 +1,476 @@
+export const PtMessages = {
+  app: {
+    name: "App PT",
+    loading: "A carregar",
+    general: {
+      not_found_page: {
+        title: "Oops! Não conseguimos encontrar o que está a procurar",
+      },
+    },
+    global_dictionary: {
+      genders: {
+        male: "Homem",
+        female: "Mulher",
+        non_binary: "Não binário",
+        non_specified: "Não especificado",
+      },
+    },
+    pages: {
+      app: {
+        settings: {
+          title: "Configurações",
+          language_selection: {
+            title: "Seleção de idioma",
+          },
+          user_profile: {
+            title: "Perfil do Usuário",
+            user: "Usuário",
+            logout: "Sair",
+            logged_user: "Usuário Logado",
+            empty_user: "Não há um usuário",
+            user_info: "Informações do Usuário",
+            roles: "Funções",
+            artist: "Artista",
+            place: "Local",
+          },
+        },
+      },
+      app_base: {
+        UsersPages: {
+          UsersDetailsPage: {
+            subpages: {
+              general: {
+                name: "Geral",
+                sections: {
+                  general: {
+                    name: "Informações Gerais",
+                    attributes: {
+                      fullname: "Nome Completo",
+                    },
+                  },
+                  contact: {
+                    name: "Contato",
+                  },
+                  emergency_contact: {
+                    name: "Contato de Emergência",
+                  },
+                },
+              },
+              arts: {
+                name: "Artes",
+                sections: {
+                  music: {
+                    name: "Música",
+                  },
+                  dance: {
+                    name: "Dança",
+                  },
+                  photography: {
+                    name: "Fotografia",
+                  },
+                  video: {
+                    name: "Vídeo",
+                  },
+                  painting: {
+                    name: "Pintura",
+                  },
+                  poetry: {
+                    name: "Poesia",
+                  },
+                  standup_comedy: {
+                    name: "Comédia Stand-up",
+                  },
+                  awards: {
+                    name: "Prêmios",
+                  },
+                },
+              },
+              memberships: {
+                name: "Associações",
+                sections: {
+                  artists: {
+                    name: "Artistas",
+                  },
+                  places: {
+                    name: "Locais",
+                  },
+                },
+              },
+              my_shows: {
+                name: "Meus Shows",
+                sections: {
+                  next_shows: {
+                    name: "Próximos shows",
+                  },
+                  past_shows: {
+                    name: "Shows passados",
+                  },
+                },
+              },
+              my_liked_shows: {
+                name: "Meus shows curtidos",
+                sections: {
+                  next_shows: {
+                    name: "Próximos shows",
+                  },
+                  past_shows: {
+                    name: "Shows passados",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      domain: {
+        CulturalAgenda: {
+          title: "Agenda Cultural",
+        },
+      },
+      ArtistsPages: {
+        ArtistsDetailsPage: {
+          subpages: {
+            general: {
+              name: "Geral",
+              sections: {
+                general: {
+                  name: "Informações gerais",
+                  attributes: {
+                    description: "",
+                    since: "Desde",
+                    home_city: "Cidade natal",
+                    categories: "Categorias",
+                    genres: "Gêneros",
+                    spoken_languages: "Línguas faladas",
+                    stage_languages: "Línguas de palco",
+                    arts_languages: "Línguas das artes",
+                  },
+                },
+                contact: {
+                  name: "Contato",
+                  attributes: {
+                    website: "Website",
+                    email: "e-mail",
+                    phone: "Telefone",
+                    mobile_phone: "Celular",
+                    whatsapp: "WhatsApp",
+                  },
+                },
+                social_networks: {
+                  name: "Redes sociais",
+                },
+                record_label: {
+                  name: "Gravadoras",
+                },
+                members: {
+                  name: "Membros",
+                },
+              },
+            },
+            arts: {
+              name: "Artes",
+              sections: {
+                discography: {
+                  name: "Discografia",
+                  attributes: {
+                    albums: "Álbuns",
+                    dvd_video: "DVD / Vídeo",
+                  },
+                },
+                media_channels: {
+                  name: "Canais de mídia",
+                },
+                gallery: {
+                  name: "Galeria",
+                  attributes: {
+                    photos: "Fotos",
+                    video: "Vídeo",
+                  },
+                },
+                awards: {
+                  name: "Prêmios",
+                },
+              },
+            },
+            social: {
+              name: "Social",
+              sections: {
+                social_network_presence: {
+                  name: "Presença em redes sociais",
+                },
+                stats: {
+                  name: "Estatísticas",
+                  attributes: {
+                    general_rate: "Taxa geral",
+                    followers: "Seguidores",
+                    event_followers: "Seguidores de eventos",
+                  },
+                },
+              },
+            },
+            shows: {
+              name: "Shows",
+              sections: {
+                next_shows: {
+                  name: "Próximos shows",
+                },
+                past_shows: {
+                  name: "Shows passados",
+                },
+              },
+            },
+          },
+        },
+      },
+      HomePage: {
+        welcome: "Bem-vindo",
+        news: "Notícias",
+        artists: "Artistas",
+        events: "Eventos",
+        places: "Lugares",
+      },
+      EventsPages: {
+        EventDetailsPage: {
+          subpages: {
+            general: {
+              name: "Informações",
+              sections: {
+                general: {
+                  name: "Informações gerais",
+                  attributes: {
+                    national_code: "Código PULEP",
+                    timetable__initial_date: "Quando?",
+                    initial_time: "Horário de início",
+                    minimumAge: "Idade mínima",
+                    timetable__openning_doors: "Abertura das portas",
+                    promoter: "Promotor",
+                  },
+                },
+                description: {
+                  name: "Descrição",
+                },
+              },
+            },
+            artists: {
+              name: "Artistas",
+              sections: {
+                main_artists: { name: "Principais artistas" },
+              },
+            },
+            place: {
+              name: "Local",
+              sections: {
+                location: {
+                  name: "Localização",
+                },
+              },
+            },
+            contact: {
+              name: "Contato e Mídia",
+              sections: {
+                contact: {
+                  name: "Informações de contato",
+                },
+                social_networks: {
+                  name: "Redes sociais",
+                },
+              },
+            },
+            extra_info: {
+              name: "Extras",
+              sections: {
+                additional_info: { name: "Informações adicionais" },
+                dress_code: { name: "Código de vestimenta" },
+                discounts: { name: "Descontos" },
+                promoter: { name: "Promotor" },
+              },
+            },
+          },
+        },
+      },
+      PlacesPages: {
+        PlacesDetailsPage: {
+          subpages: {
+            general: {
+              name: "Generale",
+              sections: {
+                gallery: {
+                  name: "Galeria",
+                },
+                general: {
+                  name: "Informações gerais",
+                  attributes: {
+                    description: "",
+                    address: "Endereço",
+                    city: "Cidade",
+                    categories: "Categorias",
+                    since: "Desde",
+                    spoken_languages: "Idiomas falados",
+                  },
+                },
+                contact: {
+                  name: "Contato",
+                  attributes: {
+                    website: "Site",
+                    email: "E-mail",
+                    phone: "Telefone",
+                    mobile_phone: "Celular",
+                    whatsapp: "WhatsApp",
+                  },
+                },
+                social_networks: {
+                  name: "Redes Sociais",
+                },
+              },
+            },
+            social: {
+              name: "Social",
+              sections: {
+                social_network_presence: {
+                  name: "Presença nas redes sociais",
+                },
+                stats: {
+                  name: "Estatísticas",
+                  attributes: {
+                    general_rate: "Avaliação geral",
+                    followers: "Seguidores",
+                    event_followers: "Seguidores de eventos",
+                  },
+                },
+              },
+            },
+            shows: {
+              name: "Shows",
+              sections: {
+                next_shows: {
+                  name: "Próximos shows",
+                },
+                past_shows: {
+                  name: "Shows passados",
+                },
+              },
+            },
+            backline: {
+              name: "Backline",
+              sections: {
+                sound_backline: {
+                  name: "Backline de som",
+                },
+                light_backline: {
+                  name: "Backline de iluminação",
+                },
+              },
+            },
+            menu: {
+              name: "Menu",
+              sections: {
+                main_course: { name: "Prato principal" },
+                second_course: { name: "Segundo prato" },
+              },
+            },
+          },
+        },
+      },
+    },
+    appbase: {
+      footer: {
+        columns: {
+          what_we_do: {
+            name: "O que fazemos?",
+            options: {
+              cultural_agenda: "Agenda Cultural",
+              for_artists: "Para artistas",
+              for_places: "Para lugares",
+              for_promoters: "Para promotores",
+              for_festivals: "Para festivais",
+            },
+          },
+          about_us: {
+            name: "Sobre nós",
+            options: {
+              history: "Nossa história",
+              press: "Imprensa",
+              career: "Carreira",
+              download: "Baixe o aplicativo",
+              data_policy: "Política de dados",
+            },
+          },
+          help: {
+            name: "Ajuda",
+            options: {
+              help_center: "Central de ajuda",
+              contact_us: "Contate-nos",
+              report: "Reportar",
+            },
+          },
+        },
+        copywrite: {
+          allRightsReserved: "Todos os direitos reservados",
+        },
+      },
+      search: {
+        empty_results: {
+          title: "Não há resultados que correspondam à sua pesquisa",
+          suggestions: {
+            statement: "Tente qualquer uma das seguintes sugestões",
+            spelling: "Verifique a ortografia",
+            less_words: "Use palavras mais genéricas ou menos palavras",
+            related_things:
+              "Navegue pelas categorias para encontrar elementos similares",
+          },
+        },
+        not_found_results: "Nenhum resultado encontrado",
+        recommendations: "Recomendações",
+        result_view_types: {
+          list: "Lista",
+          map: "Mapa",
+        },
+        results: "Resultados",
+        search: "Pesquisar",
+        search_placeholder: "Artistas, eventos, lugares...",
+        see_more: "Ver mais",
+        what_are_you_looking_for: "O que você está procurando?",
+        types: {
+          ARTISTS: "Artistas",
+          PLACES: "Lugares",
+          EVENTS: "Eventos",
+        },
+        type_your_search: "Digite sua pesquisa",
+      },
+      sidenav: {
+        name: "Menu principal",
+        sections: {
+          general: {
+            name: "Geral",
+            options: {
+              home: "Início",
+              cultural_agenda: "Agenda cultural",
+              search: "Pesquisar",
+            },
+          },
+          myInfo: {
+            name: "Minha informação",
+            options: {
+              inbox: "Caixa de entrada",
+              my_profile: "Meu perfil",
+              my_bands: "Minhas bandas",
+              my_events: "Meus eventos",
+              my_riders: "Meus riders",
+              my_places: "Meus lugares",
+            },
+          },
+          settings: {
+            name: "Configurações",
+            options: {
+              settings: "Configurações",
+              help_center: "Central de ajuda",
+              report: "Reportar",
+              send_comments: "Enviar comentários",
+              logout: "Sair",
+            },
+          },
+        },
+      },
+    },
+  },
+};

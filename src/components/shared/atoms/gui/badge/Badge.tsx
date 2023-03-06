1dbcfeccda73a3dcd2f02806ac1dc9d5745c94b3
Carlos Navarrete <navarretecarp@globalhitss.com>
2023-03-06 17:38:02 -0500

    Creación de Badge y lista de géneros

---
+++ src/components/shared/atoms/gui/badge/Badge.tsx
@@ -0,0 +1,10 @@
+import "./Badge.scss";
+
+export interface BadgeParams {
+  text: string;
+  showDeleteIcon?: boolean;
+  onClick?: Function;
+}
+export const Badge = (props: BadgeParams) => {
+  return <div className="custom-badge">{props.text}</div>;
+};

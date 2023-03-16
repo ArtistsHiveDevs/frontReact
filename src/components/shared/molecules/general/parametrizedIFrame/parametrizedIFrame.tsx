c2ae5814cd30ed0d55463ad63399b852b3444edb
Carlos Navarrete <navarretecarp@globalhitss.com>
2023-03-16 17:17:48 -0500

    [Components] ParametrizedIFrame

---
+++ src/components/shared/molecules/general/parametrizedIFrame/parametrizedIFrame.tsx
@@ -0,0 +1,25 @@
+import "./parametrizedIFrame.scss";
+
+type ParametrizedIFrameTemplate = {
+  srcUrl: string;
+  customStyles?: { [nameParam: string]: string };
+  customWidth?: string;
+  customHeight?: string;
+  title?: string;
+};
+
+export const ParametrizedIFrame: React.FC<ParametrizedIFrameTemplate> = (
+  props: ParametrizedIFrameTemplate
+) => {
+  return (
+    <iframe
+      style={props?.customStyles}
+      src={`${props.srcUrl}`}
+      width={`${props?.customWidth || "100%"}`}
+      height={`${props?.customHeight || "200px"}`}
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+      title={props?.title}
+    ></iframe>
+  );
+};

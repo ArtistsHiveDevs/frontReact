3659cbb8ab22a892468b84e42111636d60e4efbe
npcarlos <npcarlos@users.noreply.github.com>
2023-05-05 18:20:14 -0500

    Creación de consumo de Políticas (Terms & Conditions / Privacy) (#19)
    * Creación de TermsAnd Conditions
    
    * Creación de agenda cultural (#6)
    
    * Se empieza creación de agenda cultural
    
    * Fixes: Se añaden variables para dimensiones de pantalla, se comenta ancho máxmimo de pantalla para que no sea 960px, se ajustan estilos de flex card para que no se recorte en el hover
    
    * Se construye primera versión preliminar de agenda de eventosd
    
    * Se ajustan tarjetas y visualización de la agenda cultural
    
    * Ajustes en estilos de card
    
    * Modificaciones en tamaño de dia en fecha de newEntity card
    
    * Correcciones
    
    ---------
    
    Co-authored-by: SAMUEL SANTIAGO DIAZ CHAVARRO <diazss@HILDEBOG.LOCAL>
    Co-authored-by: unknown <>
    Co-authored-by: Carlos Navarrete <navarretecarp@globalhitss.com>
    
    * Avances en la página de búsqueda (#7)
    
    Co-authored-by: Carlos Navarrete <navarretecarp@globalhitss.com>
    
    * Avances en política de privacidad
    
    * Creación del componente RatingStarsView
    
    * Se añaden estadísticas y calificaciones a Places
    
    * Fixes en IconField ":", SocialNetworkWidgets
    
    * [Components] ParametrizedIFrame
    
    * Creación de cards para visualizar la oferta por tipo de Entity (#16)
    
    Co-authored-by: Carlos Navarrete <navarretecarp@globalhitss.com>
    
    ---------
    
    Co-authored-by: Carlos Navarrete <navarretecarp@globalhitss.com>
    Co-authored-by: SAMUEL SANTIAGO DIAZ CHAVARRO <diazss@HILDEBOG.LOCAL>

--- src/common/utils/redux-injectors/types.ts
+++ src/common/utils/redux-injectors/types.ts
@@ -2,7 +2,9 @@
 import { SagaInjectionModes } from "@nixjs23n6/redux-injectors";
 import { AnyAction, Reducer } from "@reduxjs/toolkit";
 import { Saga } from "redux-saga";
+import { PrivacyPolicyState } from "~/common/slices/app-base/policies/PrivacyPolicy/types";
 
+import { TermsAndConditionsState } from "~/common/slices/app-base/policies/TermsAndConditions/types";
 import { ArtistState } from "~/common/slices/artists/types";
 import { EventState } from "~/common/slices/events/types";
 import { PlaceState } from "~/common/slices/places/types";
@@ -16,6 +18,8 @@ export interface RootState {
   PlacesReducer?: PlaceState;
   SearchReducer?: SearchState;
   UsersReducer?: UserState;
+  TermsAndConditionsReducer?: TermsAndConditionsState;
+  PrivacyPolicyReducer?: PrivacyPolicyState;
 }
 
 type RequiredRootState = Required<RootState>;
---
+++ src/components/Pages/app-base/TermsAndConditions/TermsAndConditionsPage.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import { useDispatch, useSelector } from "react-redux";
+import remarkGfm from "remark-gfm";
+import { useTermsAndConditionsSlice } from "~/common/slices/app-base/policies/TermsAndConditions";
+import { selectTermsAndConditions } from "~/common/slices/app-base/policies/TermsAndConditions/selectors";
+import { TermsAndConditionsModel } from "~/models/app/policies/termsAndConditions/TermsAndConditions.model";
+
+const TermsAndConditionsPage = () => {
+  const terms: TermsAndConditionsModel = useSelector(selectTermsAndConditions);
+
+  const { actions: termsAndConditionsActions } = useTermsAndConditionsSlice();
+
+  // Hooks
+  const dispatch = useDispatch();
+
+  // Effects
+  useEffect(() => {
+    if (!terms) {
+      dispatch(
+        termsAndConditionsActions.loadTermsAndConditions({ version: "2.0" })
+      );
+    }
+  }, []);
+
+  return (
+    terms && (
+      <ReactMarkdown children={terms.terms} remarkPlugins={[remarkGfm]} />
+    )
+  );
+};
+
+export default TermsAndConditionsPage;

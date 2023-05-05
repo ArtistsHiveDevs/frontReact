fe1d06efd35614bd39038173a1e919d3118d8be5
npcarlos <npcarlos@users.noreply.github.com>
2023-05-05 18:20:14 -0500

    Creación de consumo de Políticas (Terms & Conditions / Privacy) (#19)
    * Creación de TermsAnd Conditions
    
    * Creación de agenda cultural (#6)
    
    * Se empieza creación de agenda cultural
    
    * Fixes: Se añaden variables para dimensiones de pantalla, se comenta ancho máxmimo de pantalla para que no sea 960px, se ajustan estilos de flex card para que no se recorte en el hover
    
    * Se construye primera versión preliminar de agenda de eventosd
    
    * Se ajustan tarjetas y visualización de la agenda cultural
    
    * Ajustes en estilos de card
    
    * Modificaciones en tamaño de dia en fecha de newEntity card
    
    * Correcciones
    
    ---------
    
    Co-authored-by: SAMUEL SANTIAGO DIAZ CHAVARRO <diazss@HILDEBOG.LOCAL>
    Co-authored-by: unknown <>
    Co-authored-by: Carlos Navarrete <navarretecarp@globalhitss.com>
    
    * Avances en la página de búsqueda (#7)
    
    Co-authored-by: Carlos Navarrete <navarretecarp@globalhitss.com>
    
    * Avances en política de privacidad
    
    * Creación del componente RatingStarsView
    
    * Se añaden estadísticas y calificaciones a Places
    
    * Fixes en IconField ":", SocialNetworkWidgets
    
    * [Components] ParametrizedIFrame
    
    * Creación de cards para visualizar la oferta por tipo de Entity (#16)
    
    Co-authored-by: Carlos Navarrete <navarretecarp@globalhitss.com>
    
    ---------
    
    Co-authored-by: Carlos Navarrete <navarretecarp@globalhitss.com>
    Co-authored-by: SAMUEL SANTIAGO DIAZ CHAVARRO <diazss@HILDEBOG.LOCAL>

---
+++ src/common/slices/app-base/policies/PrivacyPolicy/index.ts
@@ -0,0 +1,51 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+
+import { createSlice } from "~/common/utils/@reduxjs/toolkit";
+import {
+  useInjectReducer,
+  useInjectSaga,
+} from "~/common/utils/redux-injectors";
+import {
+  PrivacyPolicyModel,
+  PrivacyPolicyTemplate,
+} from "~/models/app/policies/privacy/PrivacyPolicy.model";
+
+import { privacyPolicySaga } from "./saga";
+import { PrivacyPolicyErrorType, PrivacyPolicyState } from "./types";
+
+export const privacyPolicyInitialState: PrivacyPolicyState = {
+  queriedTermsVersion: undefined,
+  policy: undefined,
+  loading: false,
+  error: null,
+};
+
+const slice = createSlice({
+  name: "PrivacyPolicyReducer",
+  initialState: privacyPolicyInitialState,
+  reducers: {
+    loadPrivacyPolicy(state, action?: PayloadAction<{ version: string }>) {
+      state.loading = true;
+      state.error = null;
+      state.policy = undefined;
+      state.queriedTermsVersion = action?.payload?.version;
+    },
+    privacyPolicyLoaded(state, action: PayloadAction<PrivacyPolicyTemplate>) {
+      state.policy = new PrivacyPolicyModel(action.payload);
+      state.loading = false;
+    },
+    repoError(state, action: PayloadAction<PrivacyPolicyErrorType>) {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export const { actions, reducer } = slice;
+
+export const usePrivacyPolicySlice = () => {
+  useInjectReducer({ key: slice.name, reducer: slice.reducer });
+  useInjectSaga({ key: slice.name, saga: privacyPolicySaga });
+
+  return { actions: slice.actions };
+};

34e2824de969d30cba2cb9c52015b0ad05697e86
Carlos Navarrete <navarretecarp@globalhitss.com>
2023-05-11 11:16:22 -0500

    Lectura de las ofertas en la industria MD

---
+++ src/models/domain/industryOffer/IndustryOffer.model.tsx
@@ -0,0 +1,9 @@
+import { EntityModel, EntityTemplate } from "~/models/base";
+
+export interface IndustryOfferTemplate extends EntityTemplate {
+  offer: string;
+}
+
+export class IndustryOfferModel extends EntityModel<IndustryOfferTemplate> {
+  declare offer: string;
+}

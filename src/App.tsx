4d5671cd321af76609d0175cfc4207f9a815d642
Carlos <np.carlos@gmail.com>
2023-10-29 20:49:43 -0500

    [Feat] Se agrega el footer a la app

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,52 @@ import { appMessages } from "./translations";
 
 // routes
 import { AuthProvider, HvAppContext, HvAppContextProvider } from "./common";
+import { FooterColumnTemplate } from "./components/shared/Footer/columns-menu";
+
+import FooterColumns from "./components/shared/Footer/columns-menu/ColumnsMenu";
+import FooterCopywrite from "./components/shared/Footer/footer-copywrite/FooterCopywrite";
 import SideNav from "./components/shared/sidenav";
 import { RoutesApp } from "./routes";
 
 const App = () => {
   let { lang, messages, setLang } = useContext(HvAppContext);
   const onError = (error: any) => console.log(`Error Messages: ${error}`);
+
+  // Footer
+  const footerColumns: FooterColumnTemplate[] = [
+    {
+      columnTitle: "¿Qué hacemos?",
+      options: [
+        { text: "Agenda cultural", link: "#" },
+        { text: "Para artistas", link: "#" },
+        { text: "Para sitios", link: "#" },
+        { text: "Para promotores", link: "#" },
+        { text: "Para festivales", link: "#" },
+      ],
+    },
+    {
+      columnTitle: "Nosotros",
+      options: [
+        { text: "Historia", link: "#" },
+        { text: "Prensa", link: "#" },
+        { text: "Carrera", link: "#" },
+        { text: "Descarga", link: "#" },
+        { text: "Política de datos", link: "#" },
+      ],
+    },
+    // {
+    //   columnTitle: "Proyectos",
+    //   options: [{ text: "Conoce tu país", link: "#" }],
+    // },
+    {
+      columnTitle: "¿Ayuda?",
+      options: [
+        { text: "Centro de ayuda", link: "#" },
+        { text: "Contáctanos", link: "#" },
+        { text: "Reporta", link: "#" },
+      ],
+    },
+  ];
 
   return (
     <HelmetProvider>
@@ -34,6 +74,9 @@ const App = () => {
                   <div className="content">
                     <RoutesApp />
                   </div>
+
+                  <FooterColumns footerColumns={footerColumns}></FooterColumns>
+                  <FooterCopywrite />
                 </Suspense>
               </div>
             </IntlProvider>

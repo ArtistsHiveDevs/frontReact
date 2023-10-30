144c54493790aec2de7905a5fcf9dce05a2b1c1e
Carlos <np.carlos@gmail.com>
2023-10-29 20:26:13 -0500

    [FIX] Actualización de constantes para las rutas

--- src/constants/routes.constants.ts
+++ src/constants/routes.constants.ts
@@ -1,18 +1,37 @@
 export enum PATHS {
   HOME = "/home",
-  ARTIST = "/artist",
+  SIGN_UP = "/signup",
+  LOGIN = "/login",
+  CONTACT_US = "/contact",
+  PRIVACY_POLICY = "/privacy",
+  TERMS_AND_CONDITIONS = "/terms",
+  ACADEMIES = "/academies",
+  ARTISTS = "/artists",
+  CULTURAL_AGENDA = "/agenda",
   MAIN = "/",
   EVENTS = "/events",
   NOT_FOUND = "*",
   SHOWS = "/shows",
+  INDUSTRY_OFFER = "/offer",
+  PROFILE = "/profile",
+  PLACES = "/places",
+  PROMOTERS = "/promoters",
   RIDERS = "/riders",
   SEARCH = "/search",
+  BASENAME = "/artistsHive",
+  SETTINGS = "/settings",
+  MY_FAVOURITES = "/favourites",
+  TOURS_OUTLINE = "/toursOutline",
+  REDIRECT_UNAUTHORIZED_AND_UNLOGGED_USER = "/home",
+  REDIRECT_UNAUTHORIZED_AND_LOGGED_USER = "/home",
 }
 
 export enum SUB_PATHS {
   ELEMENT_DETAILS = "details",
+  AGENDA = "agenda",
 }
 
 export enum URL_PARAMETER_NAMES {
   ELEMENT_ID = "id",
+  ROLE = "role",
 }

eca5ac140711b2b3582b4871f4c83f1f50c7d9f2
Carlos <np.carlos@gmail.com>
2023-10-29 19:51:16 -0500

    [Fix] Se llama al servicio Artists desde el Main Home

--- src/components/Pages/HomePage/MainHome/index.tsx
+++ src/components/Pages/HomePage/MainHome/index.tsx
@@ -1,44 +1,66 @@
 import { getCustomList } from "~/constants";
 
 import { ArtistModel } from "~/models/domain/artist/artist.model";
+
+import "./index.scss";
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { useArtistsSlice } from "~/common/slices";
+import { selectArtists } from "~/common/slices/artists/selectors";
+
 import MainSection from "../MainSection/MainSection";
 import WelcomeSection from "../WelcomeSection/WelcomeSection";
 import "./index.scss";
 
-const artistList: ArtistModel[] = [];
+// const artistList: ArtistModel[] = ARTISTS;
 const placesList: ArtistModel[] = [];
 const placeParams = { hidePhoto: true };
 
-const HomePage = () => (
-  <>
-    <WelcomeSection />
-    <div className="home-section-title">
-      <h1 className="welcome-title">Novedades</h1>
-    </div>
+const HomePage = () => {
+  const artistList: ArtistModel[] = useSelector(selectArtists);
+  const { actions } = useArtistsSlice();
+  const dispatch = useDispatch();
 
-    <MainSection
-      title={"Artistas"}
-      description={"Estos son los artistas nuevos más solicitados"}
-      listView={getCustomList(10, artistList)}
-      params={{ useNewCard: true }}
-    />
+  useEffect(() => {
+    if (artistList.length === 0) {
+      dispatch(actions.loadArtists());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    <MainSection
-      title={"Eventos"}
-      description={"Próximos eventos cercanos a ti"}
-      listView={getCustomList(10, artistList)}
-      params={placeParams}
-    />
+  return (
+    <>
+      <WelcomeSection />
+      <div className="home-section-title">
+        <h1 className="welcome-title">Novedades</h1>
+      </div>
 
-    <MainSection
-      title={"Lugares"}
-      description={
-        "Estos son los lugares más cercanos a tu ubicación que están buscando artistas"
-      }
-      listView={getCustomList(10, artistList)}
-      params={placeParams}
-    />
-  </>
-);
+      <MainSection
+        description={"Estos son los artistas nuevos más solicitados"}
+        listView={getCustomList(10, artistList)}
+        params={{ useNewCard: true }}
+        title={"Artistas"}
+      />
+
+      <MainSection
+        description={"Próximos eventos cercanos a ti"}
+        listView={getCustomList(10, artistList)}
+        params={placeParams}
+        title={"Eventos"}
+      />
+
+      <MainSection
+        description={
+          "Estos son los lugares más cercanos a tu ubicación que están buscando artistas"
+        }
+        listView={getCustomList(10, artistList)}
+        params={placeParams}
+        title={"Lugares"}
+      />
+    </>
+  );
+};
 
 export default HomePage;

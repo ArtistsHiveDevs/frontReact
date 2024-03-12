c78035426dba2d39d02c6db6365cda9376c79600
Carlos <np.carlos@gmail.com>
2024-03-11 23:34:50 -0500

    [Feat] Traducción de DatePicker

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,20 @@ import { IntlProvider } from "react-intl";
 import { BrowserRouter as Router } from "react-router-dom";
 
 // translations
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import "dayjs/locale/de";
+import "dayjs/locale/en";
+import "dayjs/locale/es";
+import "dayjs/locale/fr";
+import "dayjs/locale/it";
+import "dayjs/locale/pt";
 import { appMessages } from "./translations";
 
 // routes
 import { AuthProvider, HvAppContext, HvAppContextProvider } from "./common";
+
+import { deDE, enUS, esES, frFR, itIT, ptBR } from "@mui/x-date-pickers";
 
 import { AppFooter } from "~/components/shared/organisms/app/Footer/AppFooter";
 import SideNav from "~/components/shared/sidenav";
@@ -23,6 +33,8 @@ const App = () => {
     messages,
   });
   const onError = (error: any) => console.log(`Error Messages: ${error}`);
+
+  const guii18nData = geti18nGUILanguage(appLang.lang);
 
   return (
     <HelmetProvider>
@@ -39,22 +51,53 @@ const App = () => {
               messages={appLang.messages}
               onError={onError}
             >
-              <div className="wrapper">
-                <SideNav />
+              <LocalizationProvider
+                dateAdapter={AdapterDayjs}
+                adapterLocale={appLang.lang || "en"}
+                localeText={
+                  guii18nData.components.MuiLocalizationProvider.defaultProps
+                    .localeText
+                }
+              >
+                <div className="wrapper">
+                  <SideNav />
 
-                <Suspense fallback={<div>Loading...</div>}>
-                  <div className="content">
-                    <RoutesApp />
-                  </div>
-                  <AppFooter />
-                </Suspense>
-              </div>
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <div className="content">
+                      <RoutesApp />
+                    </div>
+                    <AppFooter />
+                  </Suspense>
+                </div>
+              </LocalizationProvider>
             </IntlProvider>
           </AuthProvider>
         </Router>
       </HvAppContextProvider>
     </HelmetProvider>
   );
+};
+
+const geti18nGUILanguage = (currentLang: string) => {
+  let language = enUS;
+  switch (currentLang) {
+    case "de":
+      language = deDE;
+      break;
+    case "es":
+      language = esES;
+      break;
+    case "fr":
+      language = frFR;
+      break;
+    case "it":
+      language = itIT;
+      break;
+    case "pt":
+      language = ptBR;
+      break;
+  }
+  return language;
 };
 
 export default App;

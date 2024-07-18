fe2abe173ca7dcc6cc7398126435a02a75ead00b
Carlos Navarrete <np.carlos@gmail.com>
2024-07-18 01:12:13 -0500

    [FEAT] Creación del loader

---
+++ src/components/shared/atoms/app/loader/loader-icon.tsx
@@ -0,0 +1,9 @@
+export const LoaderIcon = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <embed src="/cintas_loader.svg" type="image/svg+xml" style={{ width: '20%' }} />
+    </div>
+  );
+};
+
+export default LoaderIcon;
---
+++ src/components/shared/organisms/app/loader/loader.tsx
@@ -0,0 +1,12 @@
+import LoaderIcon from '~/components/shared/atoms/app/loader/loader-icon';
+
+export const AppLoader = () => {
+  return (
+    <div>
+      Loading...
+      <LoaderIcon />
+    </div>
+  );
+};
+
+export default AppLoader;

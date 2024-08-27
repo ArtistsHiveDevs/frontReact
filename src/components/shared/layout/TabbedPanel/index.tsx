d2c6aed46ec816e7f46bb34af0edc86595d7f2d2
Carlos <np.carlos@gmail.com>
2024-08-27 03:58:38 -0500

    [Feat] Can swipe between tabs in profile

--- src/components/shared/layout/TabbedPanel/index.tsx
+++ src/components/shared/layout/TabbedPanel/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
 import { AllowedEntityRole, RequireAuthComponent } from '~/components/shared/atoms/app/auth/RequiredAuth';
 import './index.scss';
 
@@ -21,6 +22,25 @@ export const TabbedPanel = (props: any) => {
   const changeSection = (activeSection: number) => {
     setSection(activeSection);
   };
+
+  const handlePrev = () => {
+    if (activeSectionIndex > 0) {
+      setSection(activeSectionIndex - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (activeSectionIndex < tabs?.length - 1) {
+      setSection(activeSectionIndex + 1);
+    }
+  };
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: handleNext,
+    onSwipedRight: handlePrev,
+    // preventDefaultTouchmoveEvent: true,
+    trackMouse: true, // Enable swipe with mouse for testing on desktop
+  });
 
   const tabTitles = () => {
     return tabs.map((subpage: TabbedPage, idx: number) => {
@@ -49,9 +69,9 @@ export const TabbedPanel = (props: any) => {
   };
 
   return (
-    <>
+    <div {...swipeHandlers}>
       <div className="subpages-tabs">{tabTitles()}</div>
       <div>{tabContents()}</div>
-    </>
+    </div>
   );
 };

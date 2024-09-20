eaaa45202beb88ef1023bd01415f6daea5b45960
Carlos Navarrete <npcarlos@users.noreply.github.com>
2024-09-20 01:59:56 +0000

    [FIX} Contact Page

--- src/components/Pages/app-base/ContactUs/ContactUsPage.tsx
+++ src/components/Pages/app-base/ContactUs/ContactUsPage.tsx
@@ -1,160 +1,238 @@
+import { Box, Button, TextField, Typography } from '@mui/material';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 const ContactUsPage = () => {
-  //   const markdown = `A paragraph with *emphasis* and **strong importance**.
 
-  // > A block quote with ~strikethrough~ and a URL: https://reactjs.org.
 
-  // # Título
-  // ## Título
-  // ### Título
-  // #### Título
+  //   //   const markdown = `A paragraph with *emphasis* and **strong importance**.
 
-  // * Lists
-  // * [ ] todo
-  // * [x] done
-  // * (x) hjgjsdf
+  //   // > A block quote with ~strikethrough~ and a URL: https://reactjs.org.
 
-  // A table:
+  //   // # Título
+  //   // ## Título
+  //   // ### Título
+  //   // #### Título
 
-  // | a    | b     |
-  // |------|-------|
-  // | asda | asdas |
-  // | asda | asdas |
+  //   // * Lists
+  //   // * [ ] todo
+  //   // * [x] done
+  //   // * (x) hjgjsdf
 
-  // # Another table
-  // |  A | B  | C  | D  | E  |
-  // |----|----|----|----|----|
-  // |  a | b  | c  |  d | e  |
-  // |  a | b  | c  |  d | e  |
-  // |  a | b  | c  |  d | e  |
+  //   // A table:
 
-  // `;
+  //   // | a    | b     |
+  //   // |------|-------|
+  //   // | asda | asdas |
+  //   // | asda | asdas |
 
-  const markdown = `## Acerca de nosotros
+  //   // # Another table
+  //   // |  A | B  | C  | D  | E  |
+  //   // |----|----|----|----|----|
+  //   // |  a | b  | c  |  d | e  |
+  //   // |  a | b  | c  |  d | e  |
+  //   // |  a | b  | c  |  d | e  |
 
-Con Spotify es muy fácil encontrar la música o el podcast perfectos para cada momento desde tu teléfono, computadora, tablet y muchos otros dispositivos.
+  //   // `;
 
-En Spotify hay millones de canciones y episodios. Entonces, ya sea que estés manejando, haciendo ejercicio, de fiesta o en un momento relajante, la música o el podcast perfectos siempre están a tu alcance. Elige lo que quieres escuchar o deja que Spotify te sorprenda.
+  //   const markdown = `## Acerca de nosotros
 
-Además, puedes explorar las colecciones de tus amigos, de artistas y celebridades o crear una estación de radio para relajarte y disfrutar de la música.
+  // Con Spotify es muy fácil encontrar la música o el podcast perfectos para cada momento desde tu teléfono, computadora, tablet y muchos otros dispositivos.
 
-Ponle música a tu vida con Spotify. Suscríbete o escucha música gratis.
+  // En Spotify hay millones de canciones y episodios. Entonces, ya sea que estés manejando, haciendo ejercicio, de fiesta o en un momento relajante, la música o el podcast perfectos siempre están a tu alcance. Elige lo que quieres escuchar o deja que Spotify te sorprenda.
 
-## Servicio de ayuda y atención al cliente
+  // Además, puedes explorar las colecciones de tus amigos, de artistas y celebridades o crear una estación de radio para relajarte y disfrutar de la música.
 
-1.  [Sitio de ayuda](https://support.spotify.com/). Visita nuestro sitio de ayuda para resolver tus dudas y aprender cómo aprovechar al máximo el servicio de Spotify y tu música.
-2.  [Comunidad](https://community.spotify.com/). Obtén ayuda rápida de los usuarios expertos de Spotify. Si todavía no hay una respuesta a tu pregunta, publícala y alguien te responderá a la brevedad. También puedes sugerir y votar por nuevas ideas para Spotify o, simplemente, hablar sobre música con otros fans.
-3.  [Comunícate con nosotros](https://support.spotify.com/co-es/contact-spotify-support). Comunícate con nuestro equipo de atención al cliente si no encuentras una solución en nuestro sitio de ayuda o en la Comunidad.
+  // Ponle música a tu vida con Spotify. Suscríbete o escucha música gratis.
 
-## O elige un tema:
+  // ## Servicio de ayuda y atención al cliente
 
--   ¿Quieres crear anuncios en Spotify? [Sección de anunciantes](https://ads.spotify.com/)
--   ¿Eres de la prensa y quieres hacer una consulta? [Sección de contacto para la prensa](https://newsroom.spotify.com/press-inquiries)
--   ¿Quieres postularte a un empleo? [Sección de empleos](https://www.lifeatspotify.com/)
+  // 1.  [Sitio de ayuda](https://support.spotify.com/). Visita nuestro sitio de ayuda para resolver tus dudas y aprender cómo aprovechar al máximo el servicio de Spotify y tu música.
+  // 2.  [Comunidad](https://community.spotify.com/). Obtén ayuda rápida de los usuarios expertos de Spotify. Si todavía no hay una respuesta a tu pregunta, publícala y alguien te responderá a la brevedad. También puedes sugerir y votar por nuevas ideas para Spotify o, simplemente, hablar sobre música con otros fans.
+  // 3.  [Comunícate con nosotros](https://support.spotify.com/co-es/contact-spotify-support). Comunícate con nuestro equipo de atención al cliente si no encuentras una solución en nuestro sitio de ayuda o en la Comunidad.
 
-Spotify USA, Inc. brinda el servicio de Spotify a usuarios en los Estados Unidos. Spotify AB brinda el servicio de Spotify a los usuarios en todos los otros mercados.
+  // ## O elige un tema:
 
-## Sede central de Spotify
+  // -   ¿Quieres crear anuncios en Spotify? [Sección de anunciantes](https://ads.spotify.com/)
+  // -   ¿Eres de la prensa y quieres hacer una consulta? [Sección de contacto para la prensa](https://newsroom.spotify.com/press-inquiries)
+  // -   ¿Quieres postularte a un empleo? [Sección de empleos](https://www.lifeatspotify.com/)
 
-**Spotify AB**  
-Regeringsgatan 19  
-SE-111 53 Stockholm  
-Sweden  
-Reg no: 556703-7485  
-office@spotify.com
+  // Spotify USA, Inc. brinda el servicio de Spotify a usuarios en los Estados Unidos. Spotify AB brinda el servicio de Spotify a los usuarios en todos los otros mercados.
 
-## Spotify en todo el mundo
+  // ## Sede central de Spotify
 
-**Spotify Belgium**  
-Square de Meeus 37  
-4th floor  
-1000 Brussels  
-Belgium  
-office@spotify.com
+  // **Spotify AB**  
+  // Regeringsgatan 19  
+  // SE-111 53 Stockholm  
+  // Sweden  
+  // Reg no: 556703-7485  
+  // office@spotify.com
 
-**Spotify GmbH**  
-Unter den Linden 10  
-10117 Berlin  
-Germany  
-office@spotify.com
+  // ## Spotify en todo el mundo
 
-**Spotify Canada Inc.**  
-220 Adelaide Street West  
-M5H 1W7 Toronto Ontario  
-Canada  
-office@spotify.com
+  // **Spotify Belgium**  
+  // Square de Meeus 37  
+  // 4th floor  
+  // 1000 Brussels  
+  // Belgium  
+  // office@spotify.com
 
-**Spotify Denmark ApS**  
-Vestergade 27, 1 th  
-1456 København K  
-Denmark  
-office@spotify.com
+  // **Spotify GmbH**  
+  // Unter den Linden 10  
+  // 10117 Berlin  
+  // Germany  
+  // office@spotify.com
 
-**SPOTIFY SPAIN SL**  
-Paseo de Recoletos, 7-9  
-28004 Madrid  
-Spain  
-office@spotify.com
+  // **Spotify Canada Inc.**  
+  // 220 Adelaide Street West  
+  // M5H 1W7 Toronto Ontario  
+  // Canada  
+  // office@spotify.com
 
-**Spotify Finland Oy**  
-Merimiehenkatu 36 D  
-FI-00150 Helsinki  
-Finland  
-office@spotify.com
+  // **Spotify Denmark ApS**  
+  // Vestergade 27, 1 th  
+  // 1456 København K  
+  // Denmark  
+  // office@spotify.com
 
-**Spotify France SAS**  
-48 Rue la Bruyère  
-75009  
-Paris  
-France  
-office@spotify.com
+  // **SPOTIFY SPAIN SL**  
+  // Paseo de Recoletos, 7-9  
+  // 28004 Madrid  
+  // Spain  
+  // office@spotify.com
 
-**Spotify India LLP**  
-Regus, North F/A-4,  
-Floor 1st,A Block,  
-Shivsagar Estate, Dr Annie Besant Road,  
-Worli, Mumbai Mumbai City,  
-MH 400018 INDIA  
-office@spotify.com
+  // **Spotify Finland Oy**  
+  // Merimiehenkatu 36 D  
+  // FI-00150 Helsinki  
+  // Finland  
+  // office@spotify.com
 
-**Spotify Italy S.r.l.**  
-Via Joe Colombo 4  
-20124 Milano  
-Italy  
-office@spotify.com
+  // **Spotify France SAS**  
+  // 48 Rue la Bruyère  
+  // 75009  
+  // Paris  
+  // France  
+  // office@spotify.com
 
-**Spotify Netherlands**  
-Singel 540 3h  
-1017AZ, Amsterdam  
-Netherlands  
-office@spotify.com
+  // **Spotify India LLP**  
+  // Regus, North F/A-4,  
+  // Floor 1st,A Block,  
+  // Shivsagar Estate, Dr Annie Besant Road,  
+  // Worli, Mumbai Mumbai City,  
+  // MH 400018 INDIA  
+  // office@spotify.com
 
-**Spotify Ltd**  
-Adelphi Building  
-4 Savoy Place  
-London WC2N 6AT  
-United Kingdom  
-office@spotify.com
+  // **Spotify Italy S.r.l.**  
+  // Via Joe Colombo 4  
+  // 20124 Milano  
+  // Italy  
+  // office@spotify.com
 
-**Spotify USA Inc**  
-4 World Trade Center  
-150 Greenwich Street, 62nd Floor  
-New York, NY 10007  
-USA  
-office@spotify.com
+  // **Spotify Netherlands**  
+  // Singel 540 3h  
+  // 1017AZ, Amsterdam  
+  // Netherlands  
+  // office@spotify.com
 
-**Spotify Mexico**  
-Pedregal 24 Torre Virreyes Piso 8  
-Col. Molino del Rey  
-DF 11040  
-Mexico  
-office@spotify.com
+  // **Spotify Ltd**  
+  // Adelphi Building  
+  // 4 Savoy Place  
+  // London WC2N 6AT  
+  // United Kingdom  
+  // office@spotify.com
 
-**Spotify Israel**  
-office@spotify.com`;
-  return <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />;
-};
+  // **Spotify USA Inc**  
+  // 4 World Trade Center  
+  // 150 Greenwich Street, 62nd Floor  
+  // New York, NY 10007  
+  // USA  
+  // office@spotify.com
 
-export default ContactUsPage;
+  // **Spotify Mexico**  
+  // Pedregal 24 Torre Virreyes Piso 8  
+  // Col. Molino del Rey  
+  // DF 11040  
+  // Mexico  
+  // office@spotify.com
+
+  // **Spotify Israel**  
+  // office@spotify.com`;
+  //   return <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />;
+
+  return <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '80vh',
+        padding: '20px',
+      }}
+    >
+      <Typography variant="h4" sx={{ marginBottom: '20px', fontWeight: 'bold' }}>
+        Contáctenos
+      </Typography>
+      
+      <Typography variant="body1" sx={{ marginBottom: '10px' }}>
+        Si tienes alguna pregunta o necesitas asistencia, no dudes en contactarnos. Estamos aquí para ayudarte.
+      </Typography>
+
+      <Box sx={{ marginBottom: '20px', textAlign: 'center' }}>
+        <Typography variant="h6">Soporte Técnico</Typography>
+        <Typography variant="body1">Correo electrónico: <a href="mailto:support.it@artist-hive.com">support.it@artist-hive.com</a></Typography>
+        <Typography variant="body1">Número de contacto (Llamadas): <a href="tel:+573104941440">+57 310 494 1440</a></Typography>
+        <Typography variant="body1">WhatsApp: <a href="https://wa.me/573104941440?text=Hola!">+57 310 494 1440</a></Typography>
+      </Box>
+
+      <Box sx={{ marginBottom: '20px', textAlign: 'center' }}>
+        <Typography variant="h6">Consultas Generales</Typography>
+        <Typography variant="body1">Correo electrónico: <a href="mailto:info@artist-hive.com">info@artist-hive.com</a></Typography>
+      </Box>
+{/* 
+      <Typography variant="h5" sx={{ marginBottom: '10px', fontWeight: 'bold' }}>
+        Envíanos un mensaje
+      </Typography>
+
+      <Box
+        component="form"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          maxWidth: '500px',
+        }}
+      >
+        <TextField
+          label="Nombre"
+          variant="outlined"
+          sx={{ marginBottom: '10px' }}
+          fullWidth
+        />
+        <TextField
+          label="Correo Electrónico"
+          variant="outlined"
+          sx={{ marginBottom: '10px' }}
+          fullWidth
+        />
+        <TextField
+          label="Asunto"
+          variant="outlined"
+          sx={{ marginBottom: '10px' }}
+          fullWidth
+        />
+        <TextField
+          label="Mensaje"
+          variant="outlined"
+          multiline
+          rows={4}
+          sx={{ marginBottom: '20px' }}
+          fullWidth
+        />
+        <Button variant="contained" color="primary">
+          Enviar
+        </Button>
+      </Box> */}
+    </Box>
+  };
+
+  export default ContactUsPage;

9e6f69e75273f82f7fe91eb1b0a110c3e2b270b4
Carlos <np.carlos@gmail.com>
2024-11-03 22:35:47 -0500

    [FIX] Loader fixes

--- src/components/shared/atoms/app/loader/loader-icon.tsx
+++ src/components/shared/atoms/app/loader/loader-icon.tsx
@@ -1,6 +1,18 @@
-export const LoaderIcon = () => {
+interface LoaderIconProps {
+  height?: string;
+  padding?: string;
+}
+export const LoaderIcon = (params: LoaderIconProps = {}) => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: params?.height || '100%',
+        padding: params?.padding,
+      }}
+    >
       <embed src="/cintas_loader.svg" type="image/svg+xml" style={{ width: '20%' }} />
     </div>
   );

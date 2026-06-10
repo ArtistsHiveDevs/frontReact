ef3355ad64824d22267e55ac60b6c6968ccf7284
sebasfl <s.franco2403@gmail.com>
2026-06-09 19:34:55 -0500

    feat: opencalls page v1

--- src/constants/routes.constants.ts
+++ src/constants/routes.constants.ts
@@ -32,6 +32,7 @@ export enum PATHS {
   TOURS_OUTLINE = 'toursOutline',
   FAQ = 'faq',
   PAYMENTS = 'payment',
+  OPEN_CALLS = 'open-calls',
   CC = 'cc',
   REDIRECT_UNAUTHORIZED_AND_UNLOGGED_USER = '/home',
   REDIRECT_UNAUTHORIZED_AND_LOGGED_USER = '/home',

ca72fe7fb847c092ac9971713676875b0c1b8019
Ssdiazc <samuel.santiago95@gmail.com>
2026-07-13 01:38:39 -0500

    Se ocultan secciones, se añade propiedad en entity model para la url de compartir, se añaden funciones para detectar url y para forbenv

--- src/common/utils/app-utils/app-utils.ts
+++ src/common/utils/app-utils/app-utils.ts
@@ -1,3 +1,5 @@
+import { PATHS } from "~/constants/routes.constants";
+
 export type EnvironmentType = 'prod' | 'uat' | 'sit' | 'dev';
 
 export function getEnvironment(): EnvironmentType {
@@ -12,3 +14,14 @@ export function fullyHiddenSectionsByEnvironment(forbiddenEnvironments: string[]
   const envs = Array.isArray(forbiddenEnvironments) ? forbiddenEnvironments : [forbiddenEnvironments];
   return forbiddenEnvironments?.includes(getEnvironment());
 }
+
+export function buildSharedUrl (username: string) {
+    const currentSplitPath = window.location.pathname.split('/');
+    const currentDomain = window.location.hostname;
+    currentSplitPath[currentSplitPath.length -1] = `${PATHS.USERNAME}${currentSplitPath[currentSplitPath.length -1]}`;
+    
+    const buildCustomPath = currentSplitPath?.reduce((acum, current) => acum + (current.length > 0 ? '/' + current : ''), '');
+    const buildCustomDomain = `shared.${currentDomain}`;
+    
+    return buildCustomDomain+buildCustomPath;
+};

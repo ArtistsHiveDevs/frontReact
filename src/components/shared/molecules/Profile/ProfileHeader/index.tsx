7153276036deb5ad1530f5dce77fe421d9ba66c0
CN <cndev@artist-hive.com>
2026-07-29 13:42:58 -0500

    [FIX] Se ajusta la visualización del nombre en el header fijo en un perfil personal

--- src/components/shared/molecules/Profile/ProfileHeader/index.tsx
+++ src/components/shared/molecules/Profile/ProfileHeader/index.tsx
@@ -356,7 +356,7 @@ export const ProfileHeader = (props: any) => {
           <div className="fixed-header-content">
             <AvatarWithIcon
               image={image}
-              name={element?.name}
+              name={element?.nameKnownAs || element?.name}
               avatarSize={50}
               bottomBadgeSize={30}
               buttonIcon={currentUserCanEdit && !currentUserIsInProfile && 'PiUserSwitch'}
@@ -367,7 +367,7 @@ export const ProfileHeader = (props: any) => {
               <div className="fixed-username">
                 @{element?.username} <VerifiedArtist verifiedStatus={element?.verified_status} />
               </div>
-              <div className="fixed-name">{element?.name}</div>
+              <div className="fixed-name">{element?.nameKnownAs || element?.name}</div>
             </div>
             {element && !currentUserIsInProfile && (
               <div className="fixed-like-button">
